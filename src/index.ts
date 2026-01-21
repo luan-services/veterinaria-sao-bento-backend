341da87defc0939e563d882284ea91a3a826ea0d
luan-services <luandotservices@gmail.com>
2026-01-21 01:38:06 -0300

    feat: added CORS configuration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
+
+app.use('*', cors({
+	origin: (origin, c) => {
+
+		if (process.env.NODE_ENV !== 'production') {
+			return origin || 'http://localhost:3000';
+		}
+		
+		const allowedOrigin = process.env.ALLOWED_CORS_URLS;
+		
+		// Retorna a origem se bater, ou null se não bater
+		return origin === allowedOrigin ? origin : null;
+	},
+	allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
+	credentials: true, /* must use credentials true for cookies */
+}))
 
 app.get('/', (c) => {
 	return c.text('Hello Hono!')
